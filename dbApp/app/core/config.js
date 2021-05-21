557f1ad64f954407d3f644348bef297677ac0093
Eikeike <eike@wobken.de>
2021-05-21 20:43:12 +0200

    Abgabe

--- dbApp/app/core/config.js
+++
@@ -1,1 +0,0 @@
-export const API_URI = 'http://192.168.178.64:3000'
--- server/utils/config.js
+++ server/utils/config.js
@@ -1,9 +1,9 @@
 require('dotenv').config()
 
-const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost/dbserver';
+const MONGO_URI = process.env.MONGO_URI || 'mongodb+srv://dbAdmin:dbAdminPassword@microblog.7nc4g.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 const MONGO_TEST_URI = process.env.MONGO_TEST_URI || 'mongodb://localhost/dbserver_test';
 const TOKEN_SECRET = process.env.TOKEN_SECRET || 'pvpnCCZfwOF85pBjbOebZiYIDhZ3w9LZrKwBZ7152K89mPCOHtbRlmr5Z91ci4L';
-const LISTEN_PORT = process.env.LISTEN_PORT || 3000;
+const LISTEN_PORT = process.env.PORT || 80;
 const TOKEN_EXPIRY = process.env.TOKEN_EXPIRY || '30d';
 
 module.exports = { TOKEN_SECRET, LISTEN_PORT, TOKEN_EXPIRY, MONGO_TEST_URI, MONGO_URI}
